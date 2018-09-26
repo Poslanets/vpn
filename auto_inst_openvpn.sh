f4c89d4fff16e385d99b73f39b410256138cf866
Panda_Man <poslanets@gmail.com>
2018-09-26 23:16:11 +0300

    123456

--- auto_inst_openvpn.sh
+++ auto_inst_openvpn.sh
@@ -2,14 +2,19 @@
 
 ##yum install openvpn easy-rsa -y
 mkdir -p /etc/openvpn/easy-rsa/keys
-cp -R /usr/share/easy-rsa/3.0.3/* /etc/openvpn/easy-rsa/
+cp -R /usr/share/easy-rsa/* /etc/openvpn/easy-rsa/
 cp /root/vpn/vars /etc/openvpn/easy-rsa/vars
 chmod 0755 *
-exec `cd /etc/openvpn/easy-rsa/3.0.3/`
 source ./vars
+cd /etc/openvpn/easy-rsa/
+
 
 #./clean-all
-
+./easyrsa init-pki
+./easyrsa build-ca
+./easyrsa gen-dh
+./easyrsa gen-req vpn-server nopass
+./easyrsa sign-req server vpn-server
 #Run 1 line at time.
 #./build-ca # In common name should be name [b]server[/b]
 #./build-key-server server 
@@ -23,23 +28,23 @@ sed -i 's|;duplicate-cn|duplicate-cn|' server.conf
 sed -i 's|;log         openvpn.log|log         openvpn.log|' server.conf
 sed -i 's|;user nobody|user nobody|' server.conf
 sed -i 's|;group nobody|group nobody|' server.conf
-sed -i 's|dh dh1024.pem|dh /etc/openvpn/easy-rsa/3.0.3/keys/dh2048.pem|' server.conf
+sed -i 's|dh dh1024.pem|dh /etc/openvpn/easy-rsa/keys/dh2048.pem|' server.conf
 sed -i 's|;push "redirect-gateway def1 bypass-dhcp"|push "redirect-gateway def1 bypass-dhcp"|' server.conf
-sed -i 's|ca ca.crt|ca /etc/openvpn/easy-rsa/3.0.3/keys/ca.crt|' server.conf
-sed -i 's|cert server.crt|cert /etc/openvpn/easy-rsa/3.0.3/keys/server.crt|' server.conf
-sed -i 's|key server.key|key /etc/openvpn/easy-rsa/3.0.3/keys/server.key|' server.conf
+sed -i 's|ca ca.crt|ca /etc/openvpn/easy-rsa/keys/ca.crt|' server.conf
+sed -i 's|cert server.crt|cert /etc/openvpn/easy-rsa/keys/server.crt|' server.conf
+sed -i 's|key server.key|key /etc/openvpn/easy-rsa/keys/server.key|' server.conf
 sed -i 's|;push "dhcp-option DNS 208.67.222.222"|push "dhcp-option DNS 8.8.8.8"|' server.conf
-sed -i 's|;push "dhcp-option DNS 208.67.220.220"|push "dhcp-option DNS 208.67.222.222"|' server.conf
+sed -i 's|;push "dhcp-option DNS 208.67.220.220"|push "dhcp-option DNS 8.8.4.4"|' server.conf
 sed -i 's|net.ipv4.ip_forward = 0|net.ipv4.ip_forward = 1|' /etc/sysctl.conf
-#sysctl -p
-#echo 1 > /proc/sys/net/ipv4/ip_forward
-#iptables -v -F
-#iptables -F -t mangle
-#iptables -F -t nat
-#iptables -v -A INPUT -i lo -j ACCEPT
-#iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
-#service iptables save
-#service iptables restart
-#chkconfig --add openvpn
-#chkconfig openvpn on
-#service openvpn start
+sysctl -p
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -v -F
+iptables -F -t mangle
+iptables -F -t nat
+iptables -v -A INPUT -i lo -j ACCEPT
+iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
+service iptables save
+service iptables restart
+chkconfig --add openvpn
+chkconfig openvpn on
+service openvpn start
